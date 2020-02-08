1fe6400541e2fff44cfc3ee5720925e692ccc184
kim <s158614@nishitech.ac.jp>
2020-02-08 14:18:42 +0900

    pre Loading Assets

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {AppLoading} from "expo";
+import * as Font from "expo-font";
+import {Ionicons} from "@expo/vector-icons";
+import {View, Text, StyleSheet} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+class App extends Component {
+
+  state = {
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+  handleLoaded = () => this.setState({loaded: true});
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+
+  render() {
+
+    const {loaded} = this.state;
+
+    if (loaded) {
+      return (
+          <View style={styles.container}>
+            <Text>Loaded true</Text>
+          </View>
+      );
+    }
+    else {
+      return (
+          <AppLoading
+              startAsync={this.loadAssets}
+              onFinish={this.handleLoaded}
+              onError={this.handleError}
+          />
+      )
+    }
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center"
+    }
 });
+
+export default App;
